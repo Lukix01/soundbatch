dcfcba37b8f75adc99bce7238711be5b8b9694c4
Lukix01 <e@lklos.com>
2022-10-18 08:41:20 +0200

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>SoundBatch</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
