afc02c600baa97b7ad487cf760f0c684ae0bbfac
Lukix01 <e@lklos.com>
2022-10-25 10:24:36 +0200

    login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import Login from '../components/login/Index';
+
+export default function LoginPage(): JSX.Element {
+  return (
+    <div>
+      <div className='flex h-screen'>
+        <Login />
+      </div>
+    </div>
+  );
+}
