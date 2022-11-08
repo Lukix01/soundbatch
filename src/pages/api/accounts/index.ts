142080fbc0042d8c8d9c122923659d0d23deb64c
Lukix01 <e@lklos.com>
2022-11-08 12:24:39 +0100

    get account api route

--- src/pages/api/accounts/index.ts
+++ src/pages/api/accounts/index.ts
@@ -2,18 +2,47 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '../../../lib/prisma';
 const bcrypt = require('bcrypt');
 
-export default async function Handler(request: NextApiRequest, response: NextApiResponse): Promise<void> {
-  const { method, body } = request;
+type ResponseData = {
+  msg: string;
+};
+
+export default async function Handler(request: NextApiRequest, response: NextApiResponse<ResponseData>): Promise<void> {
+  const { method, body, query } = request;
 
   switch (method) {
     case 'POST':
-      await prisma.account.create({ data: {
-        username: body.username,
-        firstName: body.firstName,
-        lastName: body.lastName,
-        password: await bcrypt.hash(body.password, 10),
-      } });
+      try {
+        await prisma.account.create({ data: {
+          username: body.username,
+          firstName: body.firstName,
+          lastName: body.lastName,
+          password: await bcrypt.hash(body.password, 10),
+        } });
+      } catch (error) {
+        console.error(error);
+      }
 
-      return response.status(201).json({ msg: 'success' });
+      return response.status(200).json({ msg: 'Successfully created an account!' });
+
+    case 'GET':
+      try {
+        const account = await prisma.account.findUnique({
+          where: {
+            username: query.username,
+          },
+        });
+        if (account) {
+          const validation = await bcrypt.compare(query.password, account.password);
+          if (validation) {
+            response.status(200).json(account);
+          } else {
+            response.status(400).json({ msg: 'Authorization failed. Wrong password.' });
+          }
+        } else {
+          response.status(400).json({ msg: 'Account not found.' });
+        }
+      } catch (error) {
+        console.error(error);
+      }
   }
 }
