b8b0026d1e68af881359fcfb3155480ebd227e38
Lukix01 <e@lklos.com>
2022-11-12 19:17:09 +0100

    redirect to login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
+import { NextRouter, useRouter } from 'next/router';
+import { useEffect } from 'react';
+import useSession from '../lib/session';
+
 export default function Home(): JSX.Element {
+  const { session, state } = useSession();
+  const router: NextRouter = useRouter();
+
+  useEffect((): void => {
+    if (!session.isLogged && state === 'loaded') {
+      router.push('/login');
+    }
+  }, [ state ]);
+
   return (
     <div>
-      <h1 className="text-4xl text-center">Hello World</h1>
+      <h1 className="text-4xl text-center">Hello {session.username}</h1>
     </div>
   );
 }
