0bf1ade6b8924f9ccfbd491094b20662a5720aa8
Lukix01 <e@lklos.com>
2022-11-12 19:15:20 +0100

    session fix

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,19 +1,30 @@
 import { useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 
-export default function useSession(): { session: { isLogged: boolean; username: string; firstName: string; lastName: string }} {
-  const [ cookies, setCookie ] = useCookies([ 'session' ]);
+export default function useSession(): {
+  session: {
+    isLogged: boolean;
+    username: string;
+    firstName: string;
+    lastName: string;
+  };
+  state: string;
+  } {
+  const [ cookies ] = useCookies([ 'session' ]);
   const [ session, setSession ] = useState({ isLogged: false, username: '', firstName: '', lastName: '' });
+  const [ state, setState ] = useState('loading');
 
-  useEffect((): void => {
-    try {
+  useEffect(() => {
+    if (cookies.session) {
       setSession({ isLogged: cookies.session && true, username: cookies.session.username,
         firstName: cookies.session.firstName,
         lastName: cookies.session.lastName,
       });
-    } catch (error) {
-      console.error(error);
     }
+
+    setState('loaded');
   }, []);
-  return { session };
+
+  return { session, state };
 }
+
