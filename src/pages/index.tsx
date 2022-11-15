1a47769d06880c8d894b6a41f06731a13c01ec9f
Lukix01 <e@lklos.com>
2022-11-15 10:28:15 +0100

    removed session verification in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,7 @@
-import { NextRouter, useRouter } from 'next/router';
-import { useEffect } from 'react';
-import useSession from '../lib/session';
-
 export default function Home(): JSX.Element {
-  const { session, state } = useSession();
-  const router: NextRouter = useRouter();
-
-  useEffect((): void => {
-    if (!session.isLogged && state === 'loaded') {
-      router.push('/login');
-    }
-  }, [ state ]);
-
   return (
     <div>
-      <h1 className="text-4xl text-center">Hello {session.username}</h1>
+      <h1 className="text-4xl text-center">Homepage</h1>
     </div>
   );
 }
