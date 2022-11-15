749227db24e34afb322fa662328c1732bd75edf0
Lukix01 <e@lklos.com>
2022-11-15 17:41:15 +0100

    FavoritesHandler

---
+++ src/pages/api/favorites/index.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../../lib/prisma';
+
+type ResponseData = {
+  msg: string;
+};
+
+export default async function FavoritesHandler(request: NextApiRequest, response: NextApiResponse<ResponseData>): Promise<void> {
+  const { method, body } = request;
+
+  switch (method) {
+    case 'POST':
+      try {
+        await prisma.account.update({
+          where: {
+            username: body.sessionUsername,
+          },
+          data: {
+            favorites: {
+              create: {
+                sound: {
+                  connect: {
+                    id: body.id,
+                  },
+                },
+              },
+            },
+          },
+        });
+      } catch (error) {
+        console.error(error);
+      }
+
+      return response.status(200).json({ msg: 'Successfully added sound to favorites!' });
+  }
+}
