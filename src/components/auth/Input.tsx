3f24d8763111ff48c86b2e29ac843ed02f48d917
Lukix01 <e@lklos.com>
2022-11-15 08:31:39 +0100

    required input

--- src/components/auth/Input.tsx
+++ src/components/auth/Input.tsx
@@ -9,7 +9,7 @@ export default function Input({ name, type, value, onchange }: Props): JSX.Eleme
   return (
     <div>
       <div className="text-slate-900 mb-1">{name}</div>
-      <input value={value} onChange={onchange} type={type} className="w-full py-1.5 border border-gray-300 focus:ring-2 ring-gray-500 ring-offset-2 px-3 rounded-lg outline-none"></input>
+      <input required value={value} onChange={onchange} type={type} className="w-full py-1.5 border border-gray-300 focus:ring-2 ring-gray-500 ring-offset-2 px-3 rounded-lg outline-none"></input>
     </div>
   );
 }
