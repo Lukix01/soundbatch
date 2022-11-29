4c5f2fa81d8d83884e579275895174535c78f5d4
Lukix01 <e@lklos.com>
2022-11-29 11:24:41 +0100

    search box width

--- src/components/library/Search.tsx
+++ src/components/library/Search.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function Search({ onchange }: Props): JSX.Element {
   return (
-    <div className="relative">
+    <div className="relative w-full">
       <MagnifyingGlassIcon className='w-5 absolute left-3 top-1/2 -translate-y-1/2'/>
       <input onChange={onchange} placeholder="Search for sounds.." className="w-full px-9 py-3 border rounded-lg outline-none focus:ring-2 ring-gray-500 ring-offset-2" />
     </div>
