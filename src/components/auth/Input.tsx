dfa6a2539c4cc1f1232c8ae985982aee843283ef
Lukix01 <e@lklos.com>
2022-12-01 22:09:37 +0100

    l

--- src/components/auth/Input.tsx
+++ src/components/auth/Input.tsx
@@ -5,11 +5,17 @@ interface Props {
   onchange?: any;
 }
 
-export default function Input({ name, type, value, onchange }: Props): JSX.Element {
+export default function Input({
+  name,
+  type,
+  value,
+  onchange,
+}: Props): JSX.Element {
   return (
     <div>
       <div className="text-slate-900 mb-1">{name}</div>
-      <input required value={value} onChange={onchange} type={type} className="w-full py-1.5 border border-gray-300 focus:ring-2 ring-gray-500 ring-offset-2 px-3 rounded-lg outline-none"></input>
+      <input required value={value} onChange={onchange} type={type} className="w-full py-1.5 border border-gray-300 focus:ring-2 ring-gray-500 ring-offset-2 px-3 rounded-lg outline-none"
+      ></input>
     </div>
   );
 }
