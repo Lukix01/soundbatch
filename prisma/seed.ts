159b5bf4c9973575ce6a1aafb381f299690ae32f
Lukix01 <e@lklos.com>
2022-12-04 20:07:34 +0000

    improved seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,21 @@
-import { readdirSync } from 'node:fs';
 import path from 'node:path';
+import { readdirSync, statSync } from 'node:fs';
 import { Sound } from '@prisma/client';
 import { prisma } from '../src/lib/prisma';
 
 async function main(): Promise<void> {
+  const soundsPath: string[] = readdirSync(path.resolve(__dirname, '../sounds/'));
+
+  const types = [];
+
+  for (const folder of soundsPath) {
+    types.push({
+      name: folder[0].toUpperCase() + folder.slice(1),
+    });
+  }
+
   await prisma.type.createMany({
-    data: [
-      {
-        name: 'Guitar',
-      },
-      {
-        name: '808',
-      },
-      {
-        name: 'Bassline',
-      },
-      {
-        name: 'Piano',
-      },
-      {
-        name: 'Drums',
-      },
-    ],
+    data: types,
   });
 
   const soundTypes = await prisma.type.findMany();
@@ -32,14 +26,16 @@ async function main(): Promise<void> {
 
   const sounds = [];
 
-  const soundsPath: string[] = readdirSync(path.resolve(__dirname, '../sounds/'));
-
   for (const folder of soundsPath) {
-    const soundTypeFolder: string[] = readdirSync(path.resolve(__dirname, '../sounds/' + folder));
+    const soundTypeFolder: string[] = readdirSync(path.resolve(__dirname, '../sounds/', folder));
 
     for (const soundFile of soundTypeFolder) {
       if (soundFile !== '.DS_Store') {
-        sounds.push({ typeId: getFromTypes(folder[0].toLocaleUpperCase() + folder.slice(1)).id, name: soundFile.slice(0, -4), size: '1 MB' });
+        sounds.push({
+          typeId: getFromTypes(folder[0].toLocaleUpperCase() + folder.slice(1)).id,
+          name: soundFile.slice(0, -4),
+          size: ((statSync(path.resolve(__dirname, '../sounds/', folder, soundFile)).size / 1_000_000)).toFixed(1) + ' MB',
+        });
       }
     }
   }
