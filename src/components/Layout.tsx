e24bf98f938a44f98c894ac5a1b507ab54e47de0
Lukix01 <e@lklos.com>
2022-12-10 15:53:52 +0100

    layout fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,14 @@
 import Sidebar from './sidebar/Index';
 
 interface Props {
-  children: JSX.Element;
+  children: React.ReactNode;
 }
 
 export default function Layout({ children }: Props): JSX.Element {
   return (
     <div className='flex h-screen'>
       <div className='w-[28rem] space-y-4 m-auto h-3/4 flex flex-col'>
-        <div className='h-full'>
-          {children}
-        </div>
+        {children}
         <Sidebar />
       </div>
     </div>
