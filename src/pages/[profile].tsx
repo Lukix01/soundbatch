011d0017998251d3e2be126d4384e95b1302d148
Lukix01 <e@lklos.com>
2022-12-10 20:20:40 +0100

    profile page

---
+++ src/pages/[profile].tsx
@@ -0,0 +1,61 @@
+import { prisma } from '../lib/prisma';
+import Layout from '../components/Layout';
+import getSession from './api/getSession';
+
+export default function ProfilePage({ account, session }: any): JSX.Element {
+  return (
+    <Layout session={session}>
+      <div className='h-full'>
+        <div className='text-center'>
+          <div className='text-xl'>{account.firstName} {account.lastName}</div>
+          <div className='text-lg'>{account.username}</div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export const getServerSideProps: (context: any) => Promise<{
+  props: {
+      account: any;
+      session: any;
+  };
+  redirect?: undefined;
+} | {
+  redirect: {
+      destination: string;
+  };
+  props?: undefined;
+}> = async (context: any): Promise<{
+  props: {
+      account: any;
+      session: any;
+  };
+  redirect?: undefined;
+} | {
+  redirect: {
+      destination: string;
+  };
+  props?: undefined;
+}> => {
+  const account = await prisma.account.findUnique({
+    where: {
+      username: context.params.profile,
+    },
+  });
+
+  let session = await getSession(context);
+
+  session = session || null;
+
+  return account ? {
+    props: {
+      account,
+      session,
+    },
+  } : {
+    redirect: {
+      destination: '/',
+    },
+  };
+};
