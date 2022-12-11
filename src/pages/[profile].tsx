0d1cd7dd553b1abf691f363fb1ef51d7d83a0e15
Lukix01 <e@lklos.com>
2022-12-11 13:48:32 +0100

    display favorite sounds in profile page

--- src/pages/[profile].tsx
+++ src/pages/[profile].tsx
@@ -1,6 +1,8 @@
+import { Account } from '@prisma/client';
 import { prisma } from '../lib/prisma';
 import { Session } from '../types';
 import Layout from '../components/Layout';
+import Sound from '../components/library/Sound';
 import getSession from './api/getSession';
 
 export default function ProfilePage({ account, session }: any): JSX.Element {
@@ -11,37 +13,41 @@ export default function ProfilePage({ account, session }: any): JSX.Element {
           <div className='text-xl'>{account.firstName} {account.lastName}</div>
           <div className='text-lg'>{account.username}</div>
         </div>
+        <div className='mt-4 space-y-4'>
+          {account.favorites.map((sound: any): JSX.Element =>
+            <Sound
+              key={sound.sound.id}
+              id={sound.sound.id}
+              type={sound.sound.type.name}
+              name={sound.sound.name}
+              extension={sound.sound.extension}
+              size={sound.sound.size}
+              downloads={sound.sound.downloads}
+              sessionUsername={session.username}
+            />,
+          )}
+        </div>
       </div>
     </Layout>
   );
 }
 
-export const getServerSideProps: (context: any) => Promise<{
-  props: {
-      account: any;
-      session: Session;
-  };
-  redirect?: undefined;
-} | {
-  redirect: {
-      destination: string;
-  };
-  props?: undefined;
-}> = async (context: any): Promise<{
-  props: {
-      account: any;
-      session: Session;
-  };
-  redirect?: undefined;
-} | {
-  redirect: {
-      destination: string;
-  };
-  props?: undefined;
-}> => {
-  const account = await prisma.account.findUnique({
+export const getServerSideProps = async (context: any) => {
+  const account: Account = await prisma.account.findUnique({
     where: {
       username: context.params.profile,
+    },
+    include: {
+      favorites: {
+        include: {
+          sound: {
+            include: {
+              type: true,
+            },
+          },
+        },
+      },
+
     },
   });
 
