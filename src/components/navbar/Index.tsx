64e043fef7eafd4ad7c7fe5cc7c81ccf5db1713e
Lukix01 <e@lklos.com>
2022-12-13 09:16:20 +0100

    navbar home button

--- src/components/navbar/Index.tsx
+++ src/components/navbar/Index.tsx
@@ -1,4 +1,4 @@
-import { ArchiveBoxIcon, UserIcon, Cog6ToothIcon } from '@heroicons/react/24/solid';
+import { HomeIcon, ArchiveBoxIcon, UserIcon } from '@heroicons/react/24/solid';
 import { NextRouter, useRouter } from 'next/router';
 import { Session } from '../../types';
 import Button from './Button';
@@ -11,9 +11,9 @@ export default function Navbar({ session }: Props): JSX.Element {
   const router: NextRouter = useRouter();
   return (
     <div className='flex mx-auto space-x-12'>
+      <Button name="/" icon={<HomeIcon />} />
       <Button name='library' icon={<ArchiveBoxIcon/>} />
       <Button name={session ? session.username : ''} icon={<UserIcon/>} path={(): Promise<boolean> => router.push(session ? session.username : '/login')} />
-      <Button name='settings' icon={<Cog6ToothIcon/>} />
     </div>
   );
 }
