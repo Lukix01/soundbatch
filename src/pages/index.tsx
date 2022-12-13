38162534a6ef746a6e92f223d34c058a33492e76
Lukix01 <e@lklos.com>
2022-12-13 19:35:38 +0100

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image';
+import { ArrowRightIcon } from '@heroicons/react/24/solid';
+import Button from '../components/home/Button';
+
 export default function Home(): JSX.Element {
   return (
-    <div>
-      <h1 className="text-4xl text-center">Homepage</h1>
+    <div className='flex h-screen'>
+      <div className='m-auto z-10 text-white flex flex-col'>
+        <div className='text-5xl text-center font-bold'>SoundBatch</div>
+        <div className='text-white text-xl text-opacity-50 mt-1'>Free to use library with samples to make good music</div>
+        <div className='mt-6 flex mx-auto space-x-4'>
+          <Button name='Get started' icon={<ArrowRightIcon/>} href='library' focused/>
+          <Button name='Samples tutorial' icon={<ArrowRightIcon/>} href='library'/>
+        </div>
+      </div>
+      <div className='bg-black absolute w-screen h-screen'/>
+      <Image src='/background.jpeg' layout='fill' className='brightness-50 blur-sm'/>
     </div>
   );
 }
