cba4daab996f30c2d9befa806a13576186314df8
Lukix01 <e@lklos.com>
2023-01-09 21:12:10 +0100

    samples tutorial page

---
+++ src/pages/tutorial.tsx
@@ -0,0 +1,11 @@
+export default function TutorialPage(): JSX.Element {
+  return (
+    <div className='flex h-screen'>
+      <div className='z-10 m-auto '>
+        <video controls width={1000} >
+          <source src='/creating_samples_tutorial.mov' />
+        </video>
+      </div>
+    </div>
+  );
+}
