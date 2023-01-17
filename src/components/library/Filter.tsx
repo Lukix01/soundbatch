b200ee91bbac24eb48185c7407a5b4124e60f823
Lukix01 <e@lklos.com>
2023-01-17 11:54:28 +0100

    void

--- src/components/library/Filter.tsx
+++ src/components/library/Filter.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export default function Filter({ filterMenu, setFilterMenu }: Props): JSX.Element {
   return (
-    <div onClick={() => setFilterMenu(!filterMenu)} className="bg-white py-3 px-8 rounded-lg border cursor-pointer ">
+    <div onClick={(): void => setFilterMenu(!filterMenu)} className="bg-white py-3 px-8 rounded-lg border cursor-pointer ">
       <div>Filter</div>
     </div>
   );
