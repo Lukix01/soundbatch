b5a778a042de19e758bac02cbc0a14ca4793a9bb
Lukix01 <e@lklos.com>
2023-02-07 08:28:39 +0100

    use herocions library

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -3,21 +3,14 @@ import Link from 'next/link';
 import { NextRouter, useRouter } from 'next/router';
 import { useState } from 'react';
 import { useCookies } from 'react-cookie';
+import { LockOpenIcon } from '@heroicons/react/24/solid';
 import Input from './Input';
-
-function UnlockIcon(): JSX.Element {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 opacity-70 m-auto group-hover:opacity-100">
-      <path d="M18 1.5c2.9 0 5.25 2.35 5.25 5.25v3.75a.75.75 0 01-1.5 0V6.75a3.75 3.75 0 10-7.5 0v3a3 3 0 013 3v6.75a3 3 0 01-3 3H3.75a3 3 0 01-3-3v-6.75a3 3 0 013-3h9v-3c0-2.9 2.35-5.25 5.25-5.25z" />
-    </svg>);
-}
 
 export default function Login(): JSX.Element {
   const [ _, setCookie ] = useCookies();
   const [ username, setUsername ] = useState<string>('');
   const [ password, setPassword ] = useState<string>('');
   const [ loginError, setLoginError ] = useState(false);
-
   const router: NextRouter = useRouter();
 
   const SignIn: (event: any) => Promise<void> = async (event: any): Promise<void> => {
@@ -48,9 +41,9 @@ export default function Login(): JSX.Element {
         </div>
         {loginError && <div className='text-red-500 text-sm mt-1'>Incorrect username or password.</div> }
         <button className='bg-gray-500 p-2.5 text-white rounded-lg w-full mt-8 flex hover:bg-gray-600 transition group'>
-          <div className='flex m-auto'>
-            <UnlockIcon />
-            <div className='ml-2'>Sign In</div>
+          <div className='flex m-auto '>
+            <LockOpenIcon className='w-4 opacity-70 group-hover:opacity-100 '/>
+            <div className='ml-2 m-auto'>Sign In</div>
           </div>
         </button>
         <div className='text-sm m-auto mt-2 text-gray-500 group cursor-pointer w-max'>Or
